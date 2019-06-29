b7eab2b15748b76a756fdd3c83882f30842169b7
am2182a <am2182a@student.american.edu>
2019-06-29 15:03:07 -0400

    WebScraping Tutorial Added

---
+++ WebScraping_Tutorial.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 29 13:44:31 2019
+
+@author: Alexander
+"""
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors. 
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+
+
+raw_html = simple_get('http://www.fabpedigree.com/james/mathmen.htm')
+html  = BeautifulSoup(raw_html, 'html.parser')
+for i, li in enumerate(html.select('li')):
+    print(i,li.text)
